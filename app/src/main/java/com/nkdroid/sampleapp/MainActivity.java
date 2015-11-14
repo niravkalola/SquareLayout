e52dc6b4610ff03e53e5d71909c3f994fc2733f4
niravkalola <niravkalola1991@gmail.com>
2015-11-14 12:26:07 +0530

    init commit

---
+++ app/src/main/java/com/nkdroid/sampleapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.nkdroid.sampleapp;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
